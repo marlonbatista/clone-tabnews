d4ced58ea0ee82e1566f2350609f4c20648f39d2
Marlon Batista <44308051+marlonbatista@users.noreply.github.com>
2024-12-20 01:18:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>Tayná, eu amo você. Se você me ama, da uma risadinha! 😁</h1>
+  return <h1>Tayná, eu amo você. Se você me ama, da uma risadinha! 😁</h1>;
 }
 
 export default Home;
-//  test para capturar novas 
