e4f5a9a0d2e00c516b713f43ae6abab1c52a3948
Marlon Batista <44308051+marlonbatista@users.noreply.github.com>
2025-01-16 20:45:43 +0000

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-const status = (request, response) => {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("SELECT 1 + 1 As sum;");
+  console.log(result.rows);
+
   response
     .status(200)
     .json({ chave: "alunos do curso.dev são acima da média" });
