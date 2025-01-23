2db7c94431a9c047b0016452b0e38817280ad87b
Marlon Batista <44308051+marlonbatista@users.noreply.github.com>
2025-01-23 22:44:54 +0000

    handle database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ const query = async (queryObject) => {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV == "development" ? false : true,
   });
   console.log("Credenciais do Postgres ", {
     host: process.env.POSTGRES_HOST,
