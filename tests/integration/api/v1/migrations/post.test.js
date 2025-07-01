b59ea04126280e770aa2e6bb1b6bbaadfbb3faa6
Marlon Batista <44308051+marlonbatista@users.noreply.github.com>
2025-07-01 22:50:13 +0000

    test: rename all descritions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,6 +6,47 @@ beforeAll(async () => {
   await cleanDataBase();
 });
 
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const schemaCurrent = await getMigrationListCurrent();
+
+        expect(response1.status).toBe(201);
+
+        const responseBody = await response1.json();
+
+        expect(responseBody.length).toBeGreaterThan(0);
+        expect(responseBody.length).toBe(schemaCurrent.length);
+
+        expect(Array.isArray(responseBody)).toBe(true);
+      });
+
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(response2Body.length).toBe(0);
+        expect(Array.isArray(response2Body)).toBe(true);
+      });
+    });
+  });
+});
+
 async function cleanDataBase() {
   await database.query("drop schema  public cascade; create schema public;");
 }
@@ -17,36 +58,3 @@ async function getMigrationListCurrent() {
 
   return result.rows[0].total_migrations;
 }
-
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  const schemaCurrent = await getMigrationListCurrent();
-
-  expect(response1.status).toBe(201);
-
-  const responseBody = await response1.json();
-
-  expect(responseBody.length).toBeGreaterThan(0);
-  expect(responseBody.length).toBe(schemaCurrent.length);
-
-  expect(Array.isArray(responseBody)).toBe(true);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  // const schemaCurrent = await getMigrationListCurrent();
-  // console.log("Schema current: " + schemaCurrent);
-
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(response2Body.length).toBe(0);
-  // expect(response2Body.length).toBe(schemaCurrent.length);
-
-  expect(Array.isArray(response2Body)).toBe(true);
-});
