543ff421e7cbc9650081ae5d057d3b9f8f5c1960
Marlon Batista <44308051+marlonbatista@users.noreply.github.com>
2025-08-29 03:36:43 +0000

    feat: add pattern handler in migration controller with next-connection

--- tests/integration/api/v1/migrations/patch.test.js
+++ tests/integration/api/v1/migrations/patch.test.js
@@ -5,21 +5,22 @@ beforeAll(async () => {
   await orchestrator.clearDatabase();
 });
 
-describe("GET /api/v1/migrations", () => {
+describe("PATCH /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations", {
-        method: "GET",
+        method: "PATCH",
       });
-
-      expect(response.status).toBe(200);
+      expect(response.status).toBe(405);
 
       const responseBody = await response.json();
 
-      expect(process.env.NODE_ENV).toBe("test");
-
-      expect(Array.isArray(responseBody)).toBe(true);
-      expect(responseBody.length).toBeGreaterThan(0);
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Method Not Allowed",
+        action: "Please check the API documentation for the allowed methods.",
+        status_code: 405,
+      });
     });
   });
 });
